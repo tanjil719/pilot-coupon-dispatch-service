f0661cb7168eec135df820d1c73db757cfa64e54
tanjilhossain719 <tanjilhossain719@gmail.com>
2026-08-10 16:36:03 +0600

    Comment out ImageFileValidator and ValidImage classes

--- src/main/java/com/pilotcoupondispatchservice/annotations/ImageFileValidator.java
+++ src/main/java/com/pilotcoupondispatchservice/annotations/ImageFileValidator.java
@@ -1,38 +1,38 @@
-package com.pilotcoupondispatchservice.annotations;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import jakarta.validation.ConstraintValidator;
-import jakarta.validation.ConstraintValidatorContext;
-
-public class ImageFileValidator implements ConstraintValidator<ValidImage, MultipartFile> {
-
-    boolean required = true;
-
-    @Override
-    public void initialize(ValidImage constraintAnnotation) {
-        required = constraintAnnotation.required();
-    }
-
-    @Override
-    public boolean isValid(MultipartFile file, ConstraintValidatorContext context) {
-
-        if (!required && file == null) {
-            return true;
-        }
-
-        if (required && file == null) {
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("Image file is missing.").addConstraintViolation();
-            return false;
-        }
-
-        String contentType = file.getContentType();
-
-        return contentType != null && isSupportedContentType(contentType);
-    }
-
-    private boolean isSupportedContentType(String contentType) {
-        return contentType.startsWith("image");
-    }
-}
+//package com.pilotcoupondispatchservice.annotations;
+//
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import jakarta.validation.ConstraintValidator;
+//import jakarta.validation.ConstraintValidatorContext;
+//
+//public class ImageFileValidator implements ConstraintValidator<ValidImage, MultipartFile> {
+//
+//    boolean required = true;
+//
+//    @Override
+//    public void initialize(ValidImage constraintAnnotation) {
+//        required = constraintAnnotation.required();
+//    }
+//
+//    @Override
+//    public boolean isValid(MultipartFile file, ConstraintValidatorContext context) {
+//
+//        if (!required && file == null) {
+//            return true;
+//        }
+//
+//        if (required && file == null) {
+//            context.disableDefaultConstraintViolation();
+//            context.buildConstraintViolationWithTemplate("Image file is missing.").addConstraintViolation();
+//            return false;
+//        }
+//
+//        String contentType = file.getContentType();
+//
+//        return contentType != null && isSupportedContentType(contentType);
+//    }
+//
+//    private boolean isSupportedContentType(String contentType) {
+//        return contentType.startsWith("image");
+//    }
+//}
--- src/main/java/com/pilotcoupondispatchservice/annotations/ValidImage.java
+++ src/main/java/com/pilotcoupondispatchservice/annotations/ValidImage.java
@@ -1,27 +1,27 @@
-package com.pilotcoupondispatchservice.annotations;
-
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE_USE;
-
-@Target(value = {FIELD, LOCAL_VARIABLE, PARAMETER, TYPE_USE})
-@Retention(value = RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = {ImageFileValidator.class})
-public @interface ValidImage {
-
-    boolean required() default true;
-
-    String message() default "Invalid image file";
-
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
-
-}
+//package com.pilotcoupondispatchservice.annotations;
+//
+//import jakarta.validation.Constraint;
+//import jakarta.validation.Payload;
+//import java.lang.annotation.Retention;
+//import java.lang.annotation.RetentionPolicy;
+//import java.lang.annotation.Target;
+//
+//import static java.lang.annotation.ElementType.FIELD;
+//import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+//import static java.lang.annotation.ElementType.PARAMETER;
+//import static java.lang.annotation.ElementType.TYPE_USE;
+//
+//@Target(value = {FIELD, LOCAL_VARIABLE, PARAMETER, TYPE_USE})
+//@Retention(value = RetentionPolicy.RUNTIME)
+//@Constraint(validatedBy = {ImageFileValidator.class})
+//public @interface ValidImage {
+//
+//    boolean required() default true;
+//
+//    String message() default "Invalid image file";
+//
+//    Class<?>[] groups() default {};
+//
+//    Class<? extends Payload>[] payload() default {};
+//
+//}
